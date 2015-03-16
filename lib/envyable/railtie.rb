850930a0e16b30c726dfd4ab1dac28f2bd3be682
Phil Nash <philnash@gmail.com>
2015-03-16 10:47:56 +0000

    Uses  instead of manually joining paths

--- lib/envyable/railtie.rb
+++ lib/envyable/railtie.rb
@@ -1,7 +1,7 @@
 module Envyable
   class Railtie < Rails::Railtie
     initializer "envyable.load", :before => :load_environment_config do
-      Envyable.load Rails.root + 'config/env.yml', Rails.env
+      Envyable.load Rails.root.join('config', 'env.yml'), Rails.env
     end
   end
 end
