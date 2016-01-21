aaac5df36072965b3f5bc470238d4e0c21448984
Mikel Kew <mikel@bikeroar.com>
2016-01-22 09:44:23 +1000

    Provide ability to load immediately via Gemfile

--- lib/envyable/railtie.rb
+++ lib/envyable/railtie.rb
@@ -1,7 +1,24 @@
+require 'envyable'
+
 module Envyable
   class Railtie < Rails::Railtie
     initializer "envyable.load", :before => :load_environment_config do
-      Envyable.load Rails.root.join('config', 'env.yml'), Rails.env
+      load
+    end
+
+    def load
+      Envyable.load root.join('config', 'env.yml'), Rails.env
+    end
+
+    # Fallback of ENV variable or current directory because
+    # Rails 4.1+ returns nil for Rails.root prior to app initialization
+    def root
+      Rails.root || Pathname.new(ENV["RAILS_ROOT"] || Dir.pwd)
+    end
+
+    # Avoid Rails calling `Kernel#load` via #method_mising
+    def self.load
+      instance.load
     end
   end
 end
