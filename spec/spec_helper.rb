f82a9033ad9bf1b3d7e6c50e2f3ad0931370778a
Phil Nash <philnash@gmail.com>
2016-10-31 13:39:59 +0000

    More style updates.
    Uses a logger to log gem difficulties in test.
    Single quotes when doubles aren't needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,20 @@
 require 'fileutils'
 require 'rubygems'
 begin
-  require "simplecov"
-  require "codeclimate-test-reporter"
+  require 'simplecov'
+  require 'codeclimate-test-reporter'
   SimpleCov.start do
     formatter SimpleCov::Formatter::MultiFormatter.new([
       SimpleCov::Formatter::HTMLFormatter,
       CodeClimate::TestReporter::Formatter
     ])
-    add_filter "/spec/"
+    add_filter '/spec/'
   end
 rescue LoadError
-  puts "Couldn't load simplecov or codeclimate-test-reporter. Check your " \
-       "dependencies."
+  require 'logger'
+  logger = Logger.new('./log/test.log')
+  logger.info 'Couldn\'t load simplecov or codeclimate-test-reporter. Check ' \
+              'your dependencies.'
 end
 
 gem 'minitest'
@@ -21,7 +23,7 @@ require 'minitest/autorun'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'envyable')
 
 def destination_root(opts={})
-  dir = File.join(File.dirname(__FILE__), "sandbox")
+  dir = File.join(File.dirname(__FILE__), 'sandbox')
   FileUtils.mkdir_p(dir)
   FileUtils.touch("#{dir}/.gitignore") if opts[:with_gitignore]
   dir
