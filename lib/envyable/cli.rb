4aca03ca949509e1c441c9be2a19c01775208548
Phil Nash <philnash@gmail.com>
2016-11-20 17:05:57 +0000

    Code style in cli and cli spec.
    Uses single quotes when doubles aren't needed.
    Uses Ruby 1.9 hash syntax.

--- lib/envyable/cli.rb
+++ lib/envyable/cli.rb
@@ -1,4 +1,4 @@
-require "thor"
+require 'thor'
 
 module Envyable
   class CLI < Thor
@@ -6,18 +6,20 @@ module Envyable
 
     namespace :envyable
 
-    desc "install", "Installs Envyable with sensible defaults"
+    desc 'install', 'Installs Envyable with sensible defaults'
     def install
-      create_file("config/env.yml")
-      create_file("config/env.yml.example")
+      create_file('config/env.yml')
+      create_file('config/env.yml.example')
       if File.exist?("#{destination_root}/.gitignore")
-        append_to_file(".gitignore") do
+        append_to_file('.gitignore') do
           "\n# Don't check in credentials \nconfig/env.yml"
         end
       end
       if File.exist?("#{destination_root}/bin/spring")
-        create_file("config/spring.rb") unless File.exist?("#{destination_root}/config/spring.rb")
-        append_to_file("config/spring.rb") do
+        unless File.exist?("#{destination_root}/config/spring.rb")
+          create_file('config/spring.rb')
+        end
+        append_to_file('config/spring.rb') do
           "\nSpring.watch 'config/env.yml'"
         end
       end
