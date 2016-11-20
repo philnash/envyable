2c2953328fea531dd2ebc09138f63fa068c4ff52
Phil Nash <philnash@gmail.com>
2016-11-20 17:22:48 +0000

    Style updates.
    Uses single quotes where possible.
    Documents the Railtie class better.

--- lib/envyable/railtie.rb
+++ lib/envyable/railtie.rb
@@ -1,11 +1,16 @@
 require 'envyable'
 
 module Envyable
+  # Internal: Automatically run Envyable.load before the Rails application loads
+  # it's environment based config.
   class Railtie < Rails::Railtie
-    initializer "envyable.load", :before => :load_environment_config do
+    # Internal: sets up the initializer to run when the Railtie is run.
+    initializer 'envyable.load', before: :load_environment_config do
       load
     end
 
+    # Internal: loads Envyable using the default config/env.yml location and the
+    # current Rails environment.
     def load
       Envyable.load root.join('config', 'env.yml'), Rails.env
     end
@@ -13,7 +18,7 @@ module Envyable
     # Fallback of ENV variable or current directory because
     # Rails 4.1+ returns nil for Rails.root prior to app initialization
     def root
-      Rails.root || Pathname.new(ENV["RAILS_ROOT"] || Dir.pwd)
+      Rails.root || Pathname.new(ENV['RAILS_ROOT'] || Dir.pwd)
     end
 
     # Avoid Rails calling `Kernel#load` via #method_mising
